f5eddaa24852a89c9aecea9cff5a87f3c859edfd
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-10-10 12:49:05 +0100

    Update 2Dmatrix.py

--- 2Dmatrix.py
+++ 2Dmatrix.py
@@ -3,6 +3,20 @@
 
 # create 2 matrix with 5 interger each
 #iterate through and print out one of the matrix using for loop
+#vectorized: print(np.matrix(A))
 
-martix_X = [[3],[5],[7],[9],[11]]
-martix_X = [[4],[6],[8],[10],[12]]
+martix_X = [[3,4,5,6,7],[5,6,7,8,9],[7,7,8,10,11],[9,6,3,77,12],[11,23,0,4,0]]
+martix_Y = [[4],[6],[8],[10],[12]]
+result = []
+
+#for i in range(len(martix_X)):
+
+
+#multiply 2 matrix: 
+for i in range(len(martix_X)):   
+   row = martix_X(i)
+   temp = []
+   for j in range(len(row)): 
+     mult = martix_X[i][j]*martix_Y[i][j]
+     temp.append(mult)
+     result.append(temp)
