a090dd3c9fdd1a696cd9b7ef50c4a17fa040caa1
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-10-10 13:40:48 +0100

    Update 2Dmatrix.py

--- 2Dmatrix.py
+++ 2Dmatrix.py
@@ -1,18 +1,21 @@
 # matrix is list of lists
-# 
 
-# create 2 matrix with 5 interger each
+# task 1: create 2 matrix with 5 interger each
 #iterate through and print out one of the matrix using for loop
 #vectorized: print(np.matrix(A))
 
 martix_X = [[3,4,5,6,7],[5,6,7,8,9],[7,7,8,10,11],[9,6,3,77,12],[11,23,0,4,0]]
 martix_Y = [[4],[6],[8],[10],[12]]
-result = []
+# print("Matrix =", matrix_X) # print as is
+
+
+
 
 #for i in range(len(martix_X)):
 
 
 #multiply 2 matrix: 
+result = []
 for i in range(len(martix_X)):   
    row = martix_X(i)
    temp = []
