552feb42f268cc2338aecd2e02eebadc5171f8d7
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-10-10 12:09:25 +0100

    Create 2Dmatrix.py

---
+++ 2Dmatrix.py
@@ -0,0 +1,8 @@
+# matrix is list of lists
+# 
+
+# create 2 matrix with 5 interger each
+#iterate through and print out one of the matrix using for loop
+
+martix_X = [[3],[5],[7],[9],[11]]
+martix_X = [[4],[6],[8],[10],[12]]
