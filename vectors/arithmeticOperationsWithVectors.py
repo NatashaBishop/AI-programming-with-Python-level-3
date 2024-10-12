66da8c02974e03997b7a0d751b7545ffab2b1f03
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-10-12 17:52:56 +0100

    Update arithmeticOperationsWithVectors.py

--- vectors/arithmeticOperationsWithVectors.py
+++ vectors/arithmeticOperationsWithVectors.py
@@ -12,11 +12,16 @@ print("Addition of two vectors: ",vctr_add)
 vctr_sub = vctr1-vctr2
 print("Subtraction of two vectors: ",vctr_sub)
 #Multiplication
+vctr_mul = vctr1*vctr2
+print("Multiplication of two vectors: ",vctr_mul)
 #Division
-
+vctr_div = vctr1/vctr2
+print("Division of two vectors: ",vctr_div)
 
 #outut
 #Output: 
 #Addition of two vectors:  [11 22 33 44 55]
 #Subtraction of two vectors:  [ 9 18 27 36 45]
-#
+#Multiplication of two vectors:  [ 10  40  90 160 250]
+#Division of two vectors: [ 10  10  10 10 10]
+
