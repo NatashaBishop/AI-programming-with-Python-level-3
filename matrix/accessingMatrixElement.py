887f0c2a5ed6dc294e71d7e1c87ad3788c592f24
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-10-13 22:03:07 +0100

    Create accessingMatrixElement.py

---
+++ matrix/accessingMatrixElement.py
@@ -0,0 +1,12 @@
+#Two dimensional array is  an array of arrays
+#data elements in two dimesnional arrays can be accessed using two indices. 
+#One index referring to the main or parent array and another index referring to the position of the data element in the inner array
+#If we use only one index then the entire inner array is printed for that index position
+
+from array import *
+T = [[11, 12, 5, 2], [15, 6,10], [10, 8, 12, 5], [12,15,8,6]]
+print(T[0]) #the whole 1st row
+print(T[1][2])
+#output
+#[11, 12, 5, 2]
+#10
