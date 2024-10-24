f7b2b88aa9a168057a49f689d16fa1335c7868ab
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-10-24 15:29:41 +0100

    Update strings.py

--- week2/strings.py
+++ week2/strings.py
@@ -1,2 +1,24 @@
 #print("sdfghjkdfghhggfd "asdfg" sdfghjkl  ") #syntax error
 print('sdfghjkdfghhggfd "asdfg" sdfghjkl  ') #ok
+print('sdfghjkdfghhgg\'s fd "asdfg" sdfghjkl  ') #ok
+# TODO: Fix this string!
+#Ford_quote  = ‘Whether you think you can, or you think you can’t—you’re right.’
+# TODO: Fix this string!
+Ford_quote  = "Whether you think you can, or you think you can\’t—you\’re right."
+print(Ford_quote)
+
+a='aaa'
+b='bbb'
+print(a+b)
+print(a+' '+b)
+
+#work with numbers as strings
+x='3'
+y='4'
+z=x+y
+print(z)
+print(int(x+y))
+print(int(x)+int(y))
+hello='hello'
+print(hello*5) #output: hellohellohellohellohello
+
