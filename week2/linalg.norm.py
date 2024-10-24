a5ad7c5fec954d2be5dc5736f2e64786e204e947
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-10-24 11:32:14 +0100

    Update linalg.norm.py

--- week2/linalg.norm.py
+++ week2/linalg.norm.py
@@ -1,6 +1,11 @@
+#liner algebra approach to compute of the magnitude of a 1D vector
 import numpy as np
 
 r1= np.array([2,3])
 mag=np.linalg.norm(r1)
 direction=np.linalg.norm(r1, ord=1)
 print(f'The Mag of [2,3] is {direction}')
+
+
+
+
