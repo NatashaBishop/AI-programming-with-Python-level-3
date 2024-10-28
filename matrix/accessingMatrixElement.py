829b61e73f0f9dbcfb3b1cb9067063d9e752e9dd
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-10-28 20:56:56 +0000

    Update accessingMatrixElement.py

--- matrix/accessingMatrixElement.py
+++ matrix/accessingMatrixElement.py
@@ -4,7 +4,7 @@
 #If we use only one index then the entire inner array is printed for that index position
 
 from array import * #import the array module, we can directly create an array like this: arr = array('i', [1, 2, 3, 4]) -Creates an array of integers
-T = [[11, 12, 5, 2], [15, 6,10], [10, 8, 12, 5], [12,15,8,6]]
+T = [[11, 12, 5, 2], [15, 6,10,14], [10, 8, 12, 5], [12,15,8,6]]
 print(T[0]) #the whole 1st row
 print(T[1][2])
 #output
