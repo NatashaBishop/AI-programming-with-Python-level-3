e6fe6f8f09bc846b48470d1b7c4be42ed86d16af
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-10-29 15:04:27 +0000

    Update 2Dmatrix.py

--- matrix/2Dmatrix.py
+++ matrix/2Dmatrix.py
@@ -6,12 +6,8 @@
 #vectorized: print(np.matrix(A))
 
 martix_X = [[3,4,5,6,7],[5,6,7,8,9],[7,7,8,10,11],[9,6,3,77,12],[11,23,0,4,0]]
-martix_Y = [[4],[6],[8],[10],[12]]
+martix_Y = [[4,4,4,4,4],[6,6,6,6,6],[8,8,8,8,8],[0,0,0,0,0],[1,1,1,1,1]]
 # print("Matrix =", matrix_X) # print as is
-
-
-
-
 #for i in range(len(martix_X)):
 
 
