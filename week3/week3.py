4ff89c5f7f14af8cf04d94d5a29ac9b86dd1205b
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-11-07 14:26:43 +0000

    Update week3.py

--- week3/week3.py
+++ week3/week3.py
@@ -1,3 +1,8 @@
+# concatenate text with number
 myName="Natalia"
-print("My name has " + str(len(myName)) +"characters")
+print("My name has " + str(len(myName)) +" characters")
 print(f"My name has {str(len(myName))} characters") #just another way
+
+#take data from user:
+inputFromUser=input("please, input your name:")
+print("Hello, "+inputFromUser)
