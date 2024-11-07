e995a29ebc6d2d253e6da18bc301112308622160
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-11-07 14:57:55 +0000

    Update sales.py

--- week3/sales.py
+++ week3/sales.py
@@ -1,1 +1,11 @@
+#Sales data given for each day as string inputs
+#calculate total namber of sales
 
+monSales="121"
+tuesSales="105"
+wedSales="110"
+thursSales="198"
+friSales="95"
+
+monSales, tuesSales, wedSales, thursSales, friSales = int(monSales), int(tuesSales), int(wedSales), int(thursSales), int(friSales) 
+print("total sales "+str(monSales+tuesSales+wedSales+thursSales+friSales))
