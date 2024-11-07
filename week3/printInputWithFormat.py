559e937d243fb9956f3b41bf49dd3c8024bf6944
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-11-07 15:40:18 +0000

    Update printInputWithFormat.py

--- week3/printInputWithFormat.py
+++ week3/printInputWithFormat.py
@@ -1,1 +1,4 @@
-name=
+name=input("what is your name?")
+surname=input("what is your surname?")
+age=input("what is your age?")
+print("Your name is {} {} and your age is {}".format(name,surname,age))
