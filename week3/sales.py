a8efdb273d30ed97e0e37b94b04c07083505c09e
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-11-07 14:58:05 +0000

    Update sales.py

--- week3/sales.py
+++ week3/sales.py
@@ -4,8 +4,11 @@
 monSales="121"
 tuesSales="105"
 wedSales="110"
-thursSales="198"
+thursSales="98"
 friSales="95"
 
+#v.1:
 monSales, tuesSales, wedSales, thursSales, friSales = int(monSales), int(tuesSales), int(wedSales), int(thursSales), int(friSales) 
-print("total sales "+str(monSales+tuesSales+wedSales+thursSales+friSales))
+print("total sales: "+str(monSales+tuesSales+wedSales+thursSales+friSales))
+#v.2:
+print(f"total sales: {int(monSales)+int(tuesSales)+int(wedSales)+int(thursSales)+int(friSales)}")
