ab5cc3ce18ac20925ce0e2de504995e1778bae2a
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-11-07 14:34:39 +0000

    Update week3.py

--- week3/week3.py
+++ week3/week3.py
@@ -4,5 +4,9 @@ print("My name has " + str(len(myName)) +" characters")
 print(f"My name has {str(len(myName))} characters") #just another way
 
 #take data from user:
-inputFromUser=input("please, input your name:")
-print("Hello, "+inputFromUser)
+#inputFromUser=input("please, input your name:")
+#print("Hello, "+inputFromUser)
+
+#calculate age from DOB:
+age=2024-int(input("WHen was you bourn?:"))
+print("Hello, your age is: "+str(age))
