df2146ebf5ee18ca1cdf95485037326802415259
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-11-14 14:29:07 +0000

    Update split.py

--- week4/split.py
+++ week4/split.py
@@ -1,2 +1,5 @@
-newStr='asdf rty 3456757'.split()
+newStr='asdf rtyasdofks irjoirj afasfasf 3456757'.split()
 print(newStr)
+newStr2='asdf rtyasdofks irjoirj afasfasf 3456757'.split(' ',3)
+#from 3rd index it stops splitting
+print(newStr2)
