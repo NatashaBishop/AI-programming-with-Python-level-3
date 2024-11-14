c5d896ee0ba8ec50012703deefc8f0e817cacd53
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-11-14 14:49:43 +0000

    Update stringMethodsPractice.py

--- week4/stringMethodsPractice.py
+++ week4/stringMethodsPractice.py
@@ -1,6 +1,7 @@
 '''Activity: String Methods Practice
 Below, we have a string variable that contains the first verse of the poem, If by Rudyard Kipling. Remember, \n is a special sequence of characters that causes a line break (a new line).
-verse = "If you can keep your head when all about you\n Are losing theirs and blaming it on you,\nIf you can trust yourself when all men doubt you,\n But make allowance for their doubting too;\nIf you can wait and not be tired by waiting,\n Or being lied about, don’t deal in lies,\nOr being hated, don’t give way to hating,\n And yet don’t look too good, nor talk too wise:"
+verse = "If you can keep your head when all about you\n Are losing theirs and blaming it on you,\nIf you can trust yourself when all men doubt you,\n But make allowance for their doubting too;
+\nIf you can wait and not be tired by waiting,\n Or being lied about, don’t deal in lies,\nOr being hated, don’t give way to hating,\n And yet don’t look too good, nor talk too wise:"
 Answer the following questions about verse
 1. What is the length of the string variable verse?
 2. What is the index of the first occurrence of the word 'and' in verse?
@@ -8,3 +9,12 @@ Answer the following questions about verse
 4. What is the count of occurrences of the word 'you' in the verse?
 You will need to refer to Python's string methods documentation.'''
 
+verse = "If you can keep your head when all about you\n Are losing theirs and blaming it on you,\nIf you can trust yourself when all men doubt you,\n But make allowance for their doubting too;\nIf you can wait and not be tired by waiting,\n Or being lied about, don't deal in lies,\nOr being hated, don't give way to hating,\n And yet don't look too good, nor talk too wise:"
+#1.the length of the string variable verse:
+print(len(verse))
+#2.the index of the first occurrence of the word 'and' in verse:
+print(verse.index('and'))
+#3.the index of the last occurrence of the word 'you' in verse:
+print(verse.rfind('and'))
+#4.the count of occurrences of the word 'you' in the verse:
+print(verse.count('you'))
