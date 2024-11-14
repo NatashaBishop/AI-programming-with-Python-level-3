a80e58924eeb2244727fd0fcec269a493716cd19
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-11-14 14:39:10 +0000

    Create stringMethodsPractice.py

---
+++ week4/stringMethodsPractice.py
@@ -0,0 +1,10 @@
+'''Activity: String Methods Practice
+Below, we have a string variable that contains the first verse of the poem, If by Rudyard Kipling. Remember, \n is a special sequence of characters that causes a line break (a new line).
+verse = "If you can keep your head when all about you\n Are losing theirs and blaming it on you,\nIf you can trust yourself when all men doubt you,\n But make allowance for their doubting too;\nIf you can wait and not be tired by waiting,\n Or being lied about, don’t deal in lies,\nOr being hated, don’t give way to hating,\n And yet don’t look too good, nor talk too wise:"
+Answer the following questions about verse
+1. What is the length of the string variable verse?
+2. What is the index of the first occurrence of the word 'and' in verse?
+3. What is the index of the last occurrence of the word 'you' in verse?
+4. What is the count of occurrences of the word 'you' in the verse?
+You will need to refer to Python's string methods documentation.'''
+
