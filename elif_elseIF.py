958d481746fbcf0c8d93316e9224ae1f86478b5e
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-11-14 15:43:08 +0000

    Update elif_elseIF.py

--- elif_elseIF.py
+++ elif_elseIF.py
@@ -1,1 +1,12 @@
 
+season=input('What season is there?').lower().title()
+
+if season=='Summer':
+    print('Water garden in the {}'.format(lower.season))
+elif season=='Winter':
+    print('It is cald in the garden')
+
+elif season=='Spring':
+    print('It is cald in the garden')
+else:
+    print('It is cold in the garden')
