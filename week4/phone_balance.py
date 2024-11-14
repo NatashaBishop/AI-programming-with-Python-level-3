f1ca4ad0ba0c8c674625ecda768590960cf8deb8
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-11-14 15:10:18 +0000

    Create phone_balance.py

---
+++ week4/phone_balance.py
@@ -0,0 +1,10 @@
+phone_balance=0
+bank_balance=100
+print(phone_balance)
+print(bank_balance)
+
+if phone_balance<5:
+    phone_balance+=10
+    bank_balance-=10
+print('You new phone balance is: {}'.format(phone_balance))
+print('You new bank balance is: {}'.format(bank_balance))
