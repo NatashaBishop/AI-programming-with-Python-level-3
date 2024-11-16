79e4c4adc7bfd7caf343160c0e2a06699ee662f3
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-11-16 21:54:15 +0000

    Create totalSales.py

---
+++ week4/totalSales.py
@@ -0,0 +1,11 @@
+#answer format: “ This week’s total sales: xxx” . 
+# #you need to take care of the spaces to get the correct answer. 
+# Don’t forget  to include a space after the colon “: “.
+
+Mon_sales = '121'
+Tues_sales = '105'
+Wed_sales = '110'
+Thurs_sales = '98'
+Fri_sales = '95'
+
+print(f' This week\'s total sales: {int(Mon_sales)+int(Tues_sales)+int(Wed_sales)+int(Thurs_sales)+int(Fri_sales)} ')
