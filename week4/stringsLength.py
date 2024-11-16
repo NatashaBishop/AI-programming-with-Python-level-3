f6c9d5194d8e4b83703f9acdbc1903793ffd9c0a
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-11-16 21:34:29 +0000

    Create stringsLength.py

---
+++ week4/stringsLength.py
@@ -0,0 +1,7 @@
+Given_name = 'William'
+Given_name = 'Bradley'
+Family_name = 'Pitt'
+print(Given_name+Given_name+Family_name)
+Name_length = len(Given_name+Given_name+Family_name)
+print(Name_length)
+print(len(Given_name+Given_name+Family_name)+2)
