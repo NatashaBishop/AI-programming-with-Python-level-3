9893c910db50597d6431108596782b45b5554ca6
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-11-21 14:38:36 +0000

    Update week5Andre.py

--- week5/week5Andre.py
+++ week5/week5Andre.py
@@ -6,8 +6,16 @@ when='fed'
 print('Dos your {} {} while {}'.format(pet,do,what,when))
 phrase='Dos your {} {} while {}'
 print(phrase.format('cat','scratch','floor','fed'))
-
-#-------------------------
+#--------------split---------
+text='asf argrrg ergferg qrwgewrg'
+print(text.split())
+#------------split3---------
+text='asf argrrg ergferg qrwgewrg'
+print(text.split(3)) # not working: TypeError: must be str or None, not int
+#------------split after index 3---------
+text='asf argrrg ergferg qrwgewrg wergrg wrwerwert'
+print(text.split(' ',3))
+#--------------input-----------
 name1=input('First name')
 name2=input('Second name')
 print('Hello, {} {}'.format(name1.capitalize(),name2.capitalize()))
