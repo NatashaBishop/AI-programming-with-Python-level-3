383ade7712a0c120a7db465f3fcdb250a82f351d
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-11-21 14:29:14 +0000

    Update week5Andre.py

--- week5/week5Andre.py
+++ week5/week5Andre.py
@@ -6,3 +6,8 @@ when='fed'
 print('Dos your {} {} while {}'.format(pet,do,what,when))
 phrase='Dos your {} {} while {}'
 print(phrase.format('cat','scratch','floor','fed'))
+
+#-------------------------
+name1=input('First name')
+name2=input('Second name')
+print('Hello, {} {}'.format(name1.capitalize(),name2.capitalize()))
