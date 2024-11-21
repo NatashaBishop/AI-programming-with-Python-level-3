aa09d50a944aca97089c3a5acd9bb96edac4c4ac
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-11-21 12:52:51 +0000

    Update week5task.py

--- week5/week5task.py
+++ week5/week5task.py
@@ -1,6 +1,8 @@
 #using np library, implement matrix multyplication of matrix O and p
 #use the dot, matmul, @ methods
-import numpy as np
+
+#install numpy from terminal: pip install numpy
+import numpy as np 
 O = [[2,5,3],[3,5,8]]
 P = [[6,5,1],[3,5,7]]
 Ovectorized=np.array(O)
