76cbdd31936f5c7c6b679b7873fa6483e2f0456b
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-11-21 12:44:19 +0000

    Update week5task.py

--- week5/week5task.py
+++ week5/week5task.py
@@ -1,10 +1,15 @@
+#using np library, implement matrix multyplication of matrix O and p
+#use the dot, matmul, @ methods
 import numpy as np
 O = [[2,5,3],[3,5,8]]
 P = [[6,5,1],[3,5,7]]
-Ovectorized=np.array[O]
-Pvectorized=np.array[P]
+Ovectorized=np.array(O)
+Pvectorized=np.array(P)
 Ptransposed=Pvectorized.T
 
 result4matmul = np.matmul(Ovectorized,Ptransposed)
 result4matmul = np.dot(Ovectorized,Ptransposed)
 result4at = Ovectorized@Ptransposed
+
+
+
