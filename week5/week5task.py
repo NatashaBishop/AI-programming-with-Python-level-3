187529574348f0cb2d0d96a7de0aa8257bac54cf
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-11-21 12:50:02 +0000

    Update week5task.py

--- week5/week5task.py
+++ week5/week5task.py
@@ -4,12 +4,12 @@ import numpy as np
 O = [[2,5,3],[3,5,8]]
 P = [[6,5,1],[3,5,7]]
 Ovectorized=np.array(O)
-Pvectorized=np.array(P)
+Pvectorized=np.array(P) # can be transposed another way: PvectorizedAndTransposed=np.array(P).T
 Ptransposed=Pvectorized.T
 
 result4matmul = np.matmul(Ovectorized,Ptransposed)
 result4matmul = np.dot(Ovectorized,Ptransposed)
 result4at = Ovectorized@Ptransposed
-
-
-
+print(result4matmul)
+print(result4matmul)
+print(result4at)
