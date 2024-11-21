6d65937fc581e2e0ad10d2717f8afde4d9ffb292
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-11-21 15:33:23 +0000

    Create pointsWinner.py

---
+++ week5/pointsWinner.py
@@ -0,0 +1,8 @@
+points=int(input('WHat is your points\' balance? '))
+                 
+if points<=50 and points>=1:
+    print('You hVE WON a wooden rabbit')
+elif points<=150 and points>51:
+    print('You hVE WON nothing')
+elif points<=200 and points>150:
+    print('You hVE WON a concert ticket')
