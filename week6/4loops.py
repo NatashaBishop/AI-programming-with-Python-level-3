7ea79bcb7dd1686d27a4fe1b36f3c0bdd124b081
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-11-28 15:10:29 +0000

    Create 4loops.py

---
+++ week6/4loops.py
@@ -0,0 +1,13 @@
+cities=['london','leverpool','grays','barking']
+titledCities=[]
+for i in cities:
+    print(i.title())
+
+for i in cities:
+    titledCities.append(i.title())
+print(titledCities)
+print(list(range(2))) #creates a list and prints first 2 indexes
+for i in range(len(cities)):
+    cities[i]=cities[i].title()
+    print(cities[i])
+print(cities)
