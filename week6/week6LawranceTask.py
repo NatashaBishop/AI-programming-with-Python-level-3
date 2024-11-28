407b7de6583fe5c7e29e094acb41d8f8d87c8bc7
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-11-28 12:39:24 +0000

    Create week6LawranceTask.py

---
+++ week6/week6LawranceTask.py
@@ -0,0 +1,14 @@
+wStarter Activity
+You were given the code below .
+def dot_product(vector_one, vector_two):
+   result= 0
+   
+   fori inrange(len(vector_one)):
+       result+= vector_one[i] * vector_two[i]
+ 
+   returnresult
+ 
+ 
+Create a Jupyter Notebook python file which:
+Create two 2 dimensional matrices
+Call the dot_product function and pass the created matrices as arguments.
