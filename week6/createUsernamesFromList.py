057189d8f6f3ba40c375ec1d218959f0bdfb4009
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-11-28 15:47:20 +0000

    Update createUsernamesFromList.py-

--- week6/createUsernamesFromList.py
+++ week6/createUsernamesFromList.py
@@ -6,3 +6,12 @@ usernames=[]
 for i in names:
         usernames.append(i.replace(' ','_').lower())
 print(usernames)
+
+
+
+#unfinished: do the same, onli with range, c scrinshot
+for i in range(len(usernames)):
+    names[i]=names[i].title()
+    print(names[i]) #prints list line by line
+print(names)
+
