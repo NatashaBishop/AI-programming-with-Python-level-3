4fc301f119e1a876a180e34f71ee346d51c6cfbc
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-12-05 15:10:37 +0000

    Create iterateTrhoughDictionary.py

---
+++ week7/iterateTrhoughDictionary.py
@@ -0,0 +1,4 @@
+#iterate and count number of fruits:
+
+basket_items = {'apples' : 4, 'oranges' : 19, 'kites' : 3, 'sandwiches' : 8}
+fruits = ['apples','oranges','pears','peaches','grapes','bananas']
