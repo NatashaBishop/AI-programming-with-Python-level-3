8198c36b8e93704d158dfaaba595888423cc97b6
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-12-05 14:40:25 +0000

    Update createList.py

--- week7/createList.py
+++ week7/createList.py
@@ -1,4 +1,5 @@
-'''Write some code, including a for loop, that iterates over a list of strings and creates a single string, html_str, which is an HTML list.  For example, if the list is 
+'''unfinished
+Write some code, including a for loop, that iterates over a list of strings and creates a single string, html_str, which is an HTML list.  For example, if the list is 
 Items = ['first string','second string'], printing html_str should output:
 <ul>
 <li>first string</li>
@@ -8,3 +9,10 @@ That is, the string's first line should be the opening tag <ul>. Following that 
 The final line of the string should be the closing tag </ul>
 items = ['first string','second string']
 html_str = "<ul>\n" #The "\n" here is the end-of-the-line char, causing chars after this in html_str to be on next line.'''
+Items = ['first string','second string']
+
+
+html_str = '<ul>\n')'
+for i in Items:
+ # html_str=html_str.append()
+ html_str+=''
