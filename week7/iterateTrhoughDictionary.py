063895b04b452604bc844c80d6bb3da9efc5ea74
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-12-05 15:23:01 +0000

    Update iterateTrhoughDictionary.py

--- week7/iterateTrhoughDictionary.py
+++ week7/iterateTrhoughDictionary.py
@@ -2,3 +2,8 @@
 result=0
 basket_items = {'apples' : 4, 'oranges' : 19, 'kites' : 3, 'sandwiches' : 8}
 fruits = ['apples','oranges','pears','peaches','grapes','bananas']
+for i in basket_items:
+    if basket_items[i]==fruits[i]:
+        result+=basket_items(value)
+        for i in fruits:
+            result +=basket_items.get(i)
