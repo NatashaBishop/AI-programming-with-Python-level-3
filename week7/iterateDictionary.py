fb5e7dd8fa730533e661088a55674048d1d926a1
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-12-05 14:53:41 +0000

    Update iterateDictionary.py

--- week7/iterateDictionary.py
+++ week7/iterateDictionary.py
@@ -3,10 +3,14 @@
 book_title = ['great',
  'expectations','the','adventures','of','sherlock','holmes','the',
  'great','gasby','hamlet','adventures','of','huckleberry','fin']
+
 wordCounter={} #create empty dictionary
 for i in book_title:
     if i not in wordCounter:
         wordCounter[i]=1
     else:
         wordCounter[i]+=1
+##################method2############ 
+for i in book_title:
+    wordCounter[i]=wordCounter.get(i,0)+1
 
