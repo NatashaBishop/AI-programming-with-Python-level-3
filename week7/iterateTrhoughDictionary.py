dadf8c0dc635a5a38f8afb131e3c0dc7f38144ca
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-12-05 15:13:02 +0000

    Update iterateTrhoughDictionary.py

--- week7/iterateTrhoughDictionary.py
+++ week7/iterateTrhoughDictionary.py
@@ -1,4 +1,4 @@
 #iterate and count number of fruits:
-
+result=0
 basket_items = {'apples' : 4, 'oranges' : 19, 'kites' : 3, 'sandwiches' : 8}
 fruits = ['apples','oranges','pears','peaches','grapes','bananas']
