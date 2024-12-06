6ddb611203c378db4616b5638382145ce868b368
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-12-06 16:24:18 +0000

    Update iterateTrhoughDictionary.py

--- week7/iterateTrhoughDictionary.py
+++ week7/iterateTrhoughDictionary.py
@@ -1,4 +1,4 @@
-#iterate and count number of fruits:
+#iterate and count number of fruits and culculet them:
 result=0
 basket_items = {'apples' : 4, 'oranges' : 19, 'kites' : 3, 'sandwiches' : 8}
 fruits = ['apples','oranges','pears','peaches','grapes','bananas']
@@ -7,3 +7,16 @@ for i in basket_items:
         if i in fruits:
             result=result+basket_items.get(i)
 print(result)
+
+#V.2 unfinished: count fruits and the rest separately:
+resultFruits=0
+resultNonFruits=0
+basket_items = {'apples' : 4, 'oranges' : 19, 'kites' : 3, 'sandwiches' : 8}
+fruits = ['apples','oranges','pears','peaches','grapes','bananas']
+
+for i in basket_items:
+        if i in fruits:
+            resultFruits=resultFruits+basket_items.get(i)
+        elif:
+            resultNonFruits=resultNonFruits+basket_items.get(i)
+print(f"resultFruits+resul: "+{resultFruits}+ "resultNonFruits"+{resultNonFruits})
