cb2652b7983130b08e80cdcd04c7855c7d87d46f
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-12-19 10:06:30 +0000

    Add files via upload

---
+++ week8/feedForwardLawrence.py
@@ -0,0 +1,7 @@
+import numpy as np
+#define the neuro network arcitecture:
+input_layer_size = 3
+hidden_layer_size = 3
+output_llayer_size = 1
+#create random input,  and waits
+input_data = np.random.rand
