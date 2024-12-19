4adf03ca1c0a7e8ffb7fbe7d20bb71aa0d01d0bf
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2024-12-19 12:55:26 +0000

    Update feedForwardLawrence.py

--- week8/feedForwardLawrence.py
+++ week8/feedForwardLawrence.py
@@ -1,7 +1,34 @@
 import numpy as np
-#define the neuro network arcitecture:
-input_layer_size = 3
+# Activation function (ReLU)
+def relu(x):
+    return np.maximum(0, x)
+#Derivative of Sigmoid
+def sigmoid(x):
+    return  1/(1 + np.exp(-x))
+# Define network architecture
+input_size = 3
 hidden_layer_size = 3
-output_llayer_size = 1
-#create random input,  and waits
-input_data = np.random.rand
+output_size = 2
+np.random.seed(42) #generate the same data every time as random value
+
+# Initialize input_data, weights and biases
+input_data = np.random.rand(3,1).T
+
+weights = [
+    np.random.randn(input_size, hidden_layer_size),
+    np.random.randn(hidden_layer_size, output_size)
+]
+biases = [
+    np.random.randn(hidden_layer_size),
+    np.random.randn(output_size)
+]
+
+# Feedforward function
+def feedforwards(inputs, weights, biases):
+    layer1_output = relu(np.add(np.dot(input_data, weights[0]), biases[0]))
+    layer2_output = sigmoid(np.add(np.dot(layer1_output, weights[1]), biases[1]))
+    return layer2_output
+
+FNN= feedforwards(input_data, weights, biases)
+print(f'The predicted value is {FNN}')
+
