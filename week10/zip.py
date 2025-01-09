7a6cd61110297f5f5fc954c975f2b9f1965f9084
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2025-01-09 14:25:05 +0000

    Update zip.py

--- week10/zip.py
+++ week10/zip.py
@@ -1,4 +1,16 @@
 items = ['BANANA', 'matresses','dogs', 'people', 'cheeses']
 weights = [22,33,44,55,66]
 print(list(zip(items,weights))) #creates list of tuples
-output: #2do: output
+#output: #do output
+
+for item, weight in zip(items,weights):
+    print(item, weight)
+#output: #do output
+
+#unpack tuple with *zip:
+manifest = [('banana', 22), ('text', 33)]
+item, weight = zip(*manifest) #manifest iz a tuple
+
+#
+for item, weight in zip(items, weights):
+    print('{}: {}'format(item, weight))
