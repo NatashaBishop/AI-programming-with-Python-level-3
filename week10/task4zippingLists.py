aacd2da3bdea16f6152c02a6c53e674a60f6862f
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2025-01-09 14:44:34 +0000

    Create task4zippingLists.py

---
+++ week10/task4zippingLists.py
@@ -0,0 +1,5 @@
+#c task description activityWeek10.png
+x_coord = [23, 53, 2, -12, 95, 103, 14, -5]
+y_coord = [677, 233, 405, 433, 905,  376,  432, 445]
+z_coord = [4, 16, -6, -42, 3, -6, 23, -1]
+labels = ["F", "J", "A", "Q", "Y", "B", "W"]
