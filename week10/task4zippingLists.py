f58bee06f8a0b06aeb50341bb21b2a1c5deeb428
NatashaBishop <51851566+NatashaBishop@users.noreply.github.com>
2025-01-10 10:59:44 +0000

    Update task4zippingLists.py

--- week10/task4zippingLists.py
+++ week10/task4zippingLists.py
@@ -1,5 +1,14 @@
-#c task description activityWeek10.png
+#c task description activityWeek10.png https://github.com/NatashaBishop/AI-programming-with-Python-level-3/blob/main/week10/activityWeek10.png
 x_coord = [23, 53, 2, -12, 95, 103, 14, -5]
 y_coord = [677, 233, 405, 433, 905,  376,  432, 445]
 z_coord = [4, 16, -6, -42, 3, -6, 23, -1]
 labels = ["F", "J", "A", "Q", "Y", "B", "W"]
+
+points =[]
+
+for i in zip(labels,x_coord,y_coord,z_coord):
+	points.append('{}:{},{},{}'.format(*i))
+
+
+for i in points:
+    print(i)
